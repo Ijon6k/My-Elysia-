e2599727a216b7d5a330371b0d2025e7c013dc91
Ijon6k <erikghafari@gmail.com>
2024-05-18 23:25:01 +0700

    Initial commit with Elysia setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello elysia")
+  .get("/hello/:name", (name) => `Hello ${name}`)
+  .get("belajar", () => "saya belajar")
+  .post("/belajar", ({ body: { kelas } }) => {
+    return { status: "mantap", kelas: kelas };
+    {
+      body: t.Object({
+        kelas: t.string({
+          minLength: 1,
+        }),
+      });
+    }
+  })
+  .get(
+    "belajar/:id",
+    ({ params: { id }, query: { nama } }) =>
+      `saya bealjar dengan params ${id} querynya adalah ${nama}`
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
